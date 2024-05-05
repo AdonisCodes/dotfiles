d5fbe8a672115faee4a7a331b122ff4eadb3c50a
Simon Ferns <business@simonferns.com>
2024-05-05 15:28:58 +0200

    [zshrc|aliases] - Added git based aliases

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -1,4 +1,14 @@
 alias ll='ls -alF'
-alias gs='git status'
 alias lvim="/Users/simonferns/.local/bin/lvim"
 alias simon-bootstrap="~/.config/zshrc/bootstrap.zsh"
+
+# GitHub Aliases
+alias gs='git status'
+alias gcm='git commit -m'
+alias gaa='git add --all'
+alias gp='git push'
+alias gpf='git push --force'
+alias gpl='git pull'
+alias gsm='git swap main'
+alias gsmp='git swap main && git pull'
+
