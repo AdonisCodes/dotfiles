dd0c3e6f3bdac9c0e47e548013c236e68308a5ba
Simon Ferns <business@simonferns.com>
2024-05-08 14:01:54 +0200

    [dotfiles|bootstrap|applications] - Added bootstrap for aws cli

--- zshrc/bootstrap.zsh
+++ zshrc/bootstrap.zsh
@@ -1,2 +1,10 @@
 brew install terminal-notifier
 brew install raycast
+
+brew tap izyumidev/xcode-discord-rpc
+brew install xcode-discord-rpc
+brew services restart xcode-discord-rpc
+
+
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "/tmp/AWSCLIV2.pkg"
+sudo installer -pkg /tmp/AWSCLIV2.pkg -target /
