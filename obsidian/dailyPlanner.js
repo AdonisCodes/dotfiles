fbad6ff9160a5a8e59d2ecb990132f9f561ffef1
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2024-07-04 21:21:43 +0200

    Update dailyPlanner.js

--- obsidian/dailyPlanner.js
+++ obsidian/dailyPlanner.js
@@ -1,1 +1,43 @@
-const a = 0; a
+let outputPage = ``;
+const currentDate = new Date();
+const referenceDate = new Date("1970-01-01T00:00:00Z");
+
+const timeDifference = currentDate.getTime() - referenceDate.getTime();
+const absoluteDay = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+const startDay = 19907;
+
+const daysSinceStartDay = absoluteDay - startDay;
+
+console.log("Days since start day:", daysSinceStartDay);
+
+function isTriangularNumber(num) {
+  let n = 1;
+  while ((n * (n + 1)) / 2 <= num) {
+    if ((n * (n + 1)) / 2 === num) {
+      return true;
+    }
+    n++;
+  }
+  return false;
+}
+
+const actionToday = isTriangularNumber(daysSinceStartDay);
+if (actionToday) {
+  outputPage += `## Toodaloo
+Practicing a hidden talent, and one that should be frowned upon!
+
+We are tapering off, hence this won't be on every daily page, just the pages that matches the triangular numbering function!
+
+- [ ] 10 Minutes
+- [ ] 1m Break
+- [ ] 10 Minutes
+- [ ] 1m Break
+- [ ] 10 Minutes
+- [ ] 1m Break
+- [ ] 10 Minutes
+- [ ] 1m Break
+- [ ] 10 Minutes`;
+}
+
+outputPage;
