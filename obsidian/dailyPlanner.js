a27057cf20bc8108b5501368f21429865a152f3f
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2024-07-05 23:00:18 +0200

    Update dailyPlanner.js

--- obsidian/dailyPlanner.js
+++ obsidian/dailyPlanner.js
@@ -1,12 +1,12 @@
 function formatNumber(num) {
-    if (num >= 1.0e+9) {
-        return (num / 1.0e+9).toFixed(3) + "B";
+    if (num >= 1.0e9) {
+        return (num / 1.0e9).toFixed(3) + "B";
     }
-    if (num >= 1.0e+6) {
-        return (num / 1.0e+6).toFixed(3) + "M";
+    if (num >= 1.0e6) {
+        return (num / 1.0e6).toFixed(3) + "M";
     }
-    if (num >= 1.0e+3) {
-        return (num / 1.0e+3).toFixed(3) + "K";
+    if (num >= 1.0e3) {
+        return (num / 1.0e3).toFixed(3) + "K";
     }
     return num.toString();
 }
@@ -22,26 +22,26 @@ const startDay = 19907;
 
 const daysSinceStartDay = absoluteDay - startDay + 1;
 
-const startDayDateString = new Date(startDay * 1000 * 60 * 60 * 24).toISOString().split("T")[0]
+const startDayDateString = new Date(startDay * 1000 * 60 * 60 * 24)
+    .toISOString()
+    .split("T")[0];
 
-outputPage += `Start Day: "${startDayDateString}"\n`
-outputPage += `Current Day: "${currentDate.toISOString().split("T")[0]}"\n`
-outputPage += `Days Since Start Day: ${daysSinceStartDay}\n`
+outputPage += `Start Day: "${startDayDateString}"\n`;
+outputPage += `Current Day: "${currentDate.toISOString().split("T")[0]}"\n`;
+outputPage += `Days Since Start Day: ${daysSinceStartDay}\n`;
 
-outputPage += "```\n"
-
+outputPage += "```\n";
 
 function isTriangularNumber(num) {
-  let n = 1;
-  while ((n * (n + 1)) / 2 <= num) {
-    if ((n * (n + 1)) / 2 === num) {
-      return true;
+    let n = 1;
+    while ((n * (n + 1)) / 2 <= num) {
+        if ((n * (n + 1)) / 2 === num) {
+            return true;
+        }
+        n++;
     }
-    n++;
-  }
-  return false;
+    return false;
 }
-
 
 // Workout Count should work like this:
 // Days from start / 2
@@ -55,14 +55,14 @@ outputPage += `### Workout Count: ${workoutCount}
 - [ ] 🧎‍♂️ (${workoutCount}x) Sit-ups 
 - [ ] 💪 (${workoutCount}x) Squats 
 - [ ] 🏋️ (${workoutCount}x) Burpees 
-- [ ] 🧘‍♀️ (${workoutCount*2}s) Plank 
+- [ ] 🧘‍♀️ (${workoutCount * 2}s) Plank 
 
-`
+`;
 
 outputPage += `### Pullup Count: ${pullupCount}
 - [ ] 🙉 (${pullupCount}x) Pullups
 
-`
+`;
 
 // Every day we should walk 5 more steps than the previous day.
 // But the app that tracks that can only go up in 100 step increments for the "Goal"
@@ -75,19 +75,130 @@ outputPage += `### Pullup Count: ${pullupCount}
 
 const startSteps = 5100;
 const stepIncrementPerDay = 5;
-let currentSteps = startSteps + (stepIncrementPerDay * daysSinceStartDay);
+let currentSteps = startSteps + stepIncrementPerDay * daysSinceStartDay;
 
 // To calculate lifetime steps we need to add up all the days since start day taking into account the increments
 let totalLifetimeSteps = 0;
 for (let i = 1; i <= daysSinceStartDay; i++) {
-  const stepsToAdd = startSteps + (stepIncrementPerDay * i)
-  totalLifetimeSteps += stepsToAdd;
+    const stepsToAdd = startSteps + stepIncrementPerDay * i;
+    totalLifetimeSteps += stepsToAdd;
 }
 outputPage += `### Daily Walking Statistics:
 - [ ] 🏃‍➡️ (${currentSteps}) Today's Steps
 - [ ] 🏃‍♀️ (${formatNumber(totalLifetimeSteps)}) Total Lifetime Steps
 
-`
+`;
+const learningDailes = [
+    "General Keyboard Shortcuts","Learning Plan","Learning Plan","Obsidian Improvements","Learning Plan","Learning Plan","Learning Plan",
+    "General Keyboard Shortcuts","Learning Plan","Learning Plan","Obsidian Improvements","Learning Plan","Obsidian Improvements","Learning Plan",
+    "General Keyboard Shortcuts","Learning Plan","Learning Plan","Obsidian Improvements","Learning Plan","Learning Plan","Learning Plan",
+    "General Keyboard Shortcuts","Learning Plan","Learning Plan","Obsidian Improvements","Learning Plan","Obsidian Improvements","Obsidian Improvements",
+    "General Keyboard Shortcuts","Learning Plan","Learning Plan"
+]
+
+const currentDayOfMonth = currentDate.getDate();
+
+const learningItem = learningDailes[currentDayOfMonth - 1];
+
+const dailySchedule = `---
+
+7am - 8am
+- Learning ${learningItem}
+
+---
+
+8am - 8:20am
+- 20m
+    - [ ] Put Bed away (5m)
+    - [ ] New Clothes (4m)
+    - [ ] Wash Hands (2m)
+    - [ ] Cup of Caffeine + Salt Water (2m)
+    - [ ] Brush Teeth (2m)
+    - [ ] Batch Clothes + Blankey into Washing Machine (5m)
+
+---
+
+8:20am - 12:20pm
+- [ ] Freelancing Work
+
+---
+
+12:20pm - 13:00pm
+- [ ] 20m - Running (${currentSteps} steps) 👆
+- [ ] 10m - 300 Calories
+- [ ] 10m - ${daysSinceStartDay % 2 == 0 ? "🫧 Mop Floor" : "🧹 Sweep Floor"}
+
+---
+
+1pm - 1:30pm
+- 10m
+    - [ ] Cold Shower (3m)
+    - [ ] Clothes (3m)
+    - [ ] Hair (1m)
+    - [ ] Deodorant (1m)
+    - [ ] Nose + Ears (1m)
+    - [ ] Spray (1m)
+- 10m
+    - [ ] Clean Phone (2m)
+    - [ ] Clean Laptop (3m)
+    - [ ] Clean Keyboard (2m)
+    - [ ] Clean Table (3m)
+- [ ] 10m - Do things to drive
+
+---
+
+1:30pm - 2:30pm
+- 1h
+    - [ ] Read English (5m)
+    - [ ] Read Afrikaans (5m)
+    - [ ] Duolingo (5m)
+    - [ ] Meditate (10m)
+    - [ ] Touchtype (10m)
+    - [ ] Workout (10m)
+    - [ ] Brain Dumping (5m)
+    - [ ] Learn new Word (5m)
+    - [ ] Measure Weight (2m)
+    - [ ] Claim Sweat + Sweatcoin (3m)
+
+---
+
+2:30pm - 4:30pm
+- [ ] 2h - Automa
+
+---
+
+4:30pm - 5:30pm
+- [ ] 1h - Learning ${learningItem}
+
+---
+
+5:30pm - 6:30pm
+- [ ] 30m - Food
+- [ ] 10m - Cold Shower
+- [ ] 10m - Wash walls
+- [ ] 10m - Prep Meeting
+
+---
+
+6:30pm - 7:00pm
+- [ ] 30m - Meeting
+
+---
+
+7:00pm - 8:00pm
+- [ ] Content Creation
+
+---
+
+8:00pm - 11:30pm
+- [ ] 3.5h Freelancing Work
+- [ ] 10m Make Bed
+
+---
+
+`;
+
+outputPage += dailySchedule;
 
 // Next day Planning List
 outputPage += `### Next Day Planning
@@ -114,12 +225,11 @@ This is applicable from around **11:30pm - 12:00am**, the planning will prepare 
     - [ ] 👖 Trousers
     - [ ] 👟 Shoes
 
-`
-
+`;
 
 const actionToday = isTriangularNumber(daysSinceStartDay);
 if (actionToday) {
-  outputPage += `### F/ Today is the day:
+    outputPage += `### F/ Today is the day:
 - [ ] 💨 5 Minutes
 - [ ] 🐢 1 Minute
 - [ ] 💨 5 Minutes
