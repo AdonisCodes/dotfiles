588fe3f09fe5adc607c3753042d6b7637ea8575e
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2024-07-06 15:53:35 +0200

    Update dailyRandomWord.js

--- obsidian/dailyRandomWord.js
+++ obsidian/dailyRandomWord.js
@@ -1,22 +1,46 @@
+async function main() {
+  const randomWord = await fetch("https://random-word-api.vercel.app/api?words=1")
+    .then(response => response.json())
+    .then(data => data[0]);
 
-const randomWord = fetch("https://random-word-api.vercel.app/api?words=1")
-  .then((response) => response.json())
-  .then((data) => data[0]);
+  const article = await fetch(`https://en.wikipedia.org/api/rest_v1/page/html/${randomWord}`)
+    .then(response => response.text())
+    .then(html => {
+      const fixedHtml = html.replace(/="\/\/upload.wikimedia/g, '="http://upload.wikimedia');
+      return fixedHtml;
+    })
+    .catch(error => {
+      console.error('Error fetching or processing the HTML:', error);
+      return '';
+    });
 
-const article = fetch(`https://en.wikipedia.org/api/rest_v1/page/html/${randomWord}`)
-.then(response => response.text())
-.then(html => {
-  const fixedHtml = html.replace(/="\/\/upload.wikimedia/g, '="http://upload.wikimedia');
-  console.log(fixedHtml);
-})
-.catch(error => console.error('Error fetching or processing the HTML:', error));
-
-const page = `
+  const page = `
 # ${randomWord}
 
 # Wikipedia
 ${article}
-`
+`;
 
-console.log(page)
-page;
+  console.log(page);
+
+  if (article.length < 3000) {
+    return "";
+  }
+
+  return page;
+}
+
+async function generatePage() {
+  let page = "";
+  while (true) {
+    const page_ = await main();
+    if (page_) {
+      page += page_;
+      break;
+    }
+  }
+  console.log(page);
+  return page;
+}
+
+generatePage();
