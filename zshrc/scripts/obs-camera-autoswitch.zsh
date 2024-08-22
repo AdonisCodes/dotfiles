56e4b47f7b77359428c455de512f590a018bcb56
Simon Ferns <business@simonferns.com>
2024-08-22 10:02:24 +0200

    add

--- zshrc/scripts/obs-camera-autoswitch.zsh
+++ zshrc/scripts/obs-camera-autoswitch.zsh
@@ -6,12 +6,14 @@ FLAG_FILENAME="obs-toggle-flag"
 while true; do
     if [ "$(ls $FLAG_DIR | grep "$FLAG_FILENAME")" ]; then
         echo "WORKING: SWITCHING CAMERA FACING"
-        /Users/simonferns/.local/bin/obs-cli scene switch "Front Cam"
+        /Users/simonferns/.local/bin/obs-cli item show camera-record ; /Users/simonferns/.local/bin/obs-cli item show screen-record
         echo "WORKING: SWITCHED TO FRONT FACING"
         sleep 10
-        /Users/simonferns/.local/bin/obs-cli scene switch "Side Cam"
+        /Users/simonferns/.local/bin/obs-cli item hide camera-record ; /Users/simonferns/.local/bin/obs-cli item hide screen-record
+        /Users/simonferns/.local/bin/obs-cli item show screen-record-2 ; /Users/simonferns/.local/bin/obs-cli item show camera-record-2
         echo "WORKING: SWITCHED TO SIDE FACING"
-        sleep 10
+        sleep 20
+        /Users/simonferns/.local/bin/obs-cli item hide screen-record-2 ; /Users/simonferns/.local/bin/obs-cli item hide camera-record-2
     else
         echo "WAITING: TOGGLE TOGGLED TO FALSE. WAITING..."
         sleep 1
